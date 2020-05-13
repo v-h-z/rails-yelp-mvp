850ab4738e2edb28c1f0d6906f68d72e96ca8b99
victor holl <v_h@me.com>
2020-05-13 14:48:06 +0200

    everything is working fine

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
