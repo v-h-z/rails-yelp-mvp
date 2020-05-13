e12ca54533c976130b899f174822caacb31f25e1
victor holl <v_h@me.com>
2020-05-13 12:03:18 +0200

    with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'firing up'
+5.times do
+  resto = Restaurant.new
+  resto.name = Faker::Restaurant.name
+  resto.address = Faker::Address.full_address
+  resto.phone_number = Faker::PhoneNumber.phone_number
+  resto.category = %w[chinese italian japanese french belgian].sample
+  resto.save
+  puts "created #{resto.name}"
+end
+puts 'done'
